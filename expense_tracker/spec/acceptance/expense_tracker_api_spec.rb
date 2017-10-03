0ee1cce707e6f2345c08d0d2fe20cdedb36de56e
Alex <alexgascon.93@gmail.com>
2017-10-03 12:52:31 +0200

    Completing first test + Improving app API

--- expense_tracker/spec/acceptance/expense_tracker_api_spec.rb
+++ expense_tracker/spec/acceptance/expense_tracker_api_spec.rb
@@ -1,5 +1,6 @@
 require 'rack/test'
 require 'json'
+require_relative '../../app/api'
 
 module ExpenseTracker
 	RSpec.describe 'Expense Tracker API' do
@@ -9,14 +10,43 @@ module ExpenseTracker
 			ExpenseTracker::API.new
 		end
 
+		def post_expense(expense)
+			post '/expenses', JSON.generate(expense)
+			expect(last_response.status).to eq(200)
+
+			parsed = JSON.parse(last_response.body)
+			expected = { "expense_id" => a_kind_of(Integer) }
+			expect(parsed).to include(expected)
+
+			expense.merge('id': parsed['expense_id'])
+		end
+
 		it 'records submitted expenses' do
-			coffee = {
+			pending 'Need to persist expenses'
+
+			coffee = post_expense(
 				'payee': 'Starbucks',
 				'amount': 5.75,
 				'date': '2017-06-10'
-			}
+			)
 
-			post '/expenses', JSON.generate(coffee)
+			zoo = post_expense(
+				'payee': 'Zoo',
+				'amount': 15.25,
+				'date': '2017-06-10'
+			)
+
+			groceries = post_expense(
+				'payee': 'Whole Foods',
+				'amount': 95.20,
+				'date': '2017-06-11'
+			)
+
+		  get '/expenses/2017-06-10'
+		  expect(last_response.status).to eq(200)
+
+		  expenses = JSON.parse(last_response.body)
+		  expect(expenses).to contain_exactly(coffee, zoo)
 		end
 	end
 end
