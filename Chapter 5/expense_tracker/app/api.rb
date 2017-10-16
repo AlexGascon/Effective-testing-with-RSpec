c7009f60db9c6043baf5ed91bb7df359e8b27036
Alex <alexgascon.93@gmail.com>
2017-10-16 18:16:54 +0200

    Mocking Storage behaviour and passing first API tests

--- Chapter 5/expense_tracker/app/api.rb
+++ Chapter 5/expense_tracker/app/api.rb
@@ -3,8 +3,18 @@ require 'json'
 
 module ExpenseTracker
 	class API < Sinatra::Base
+		def initialize(ledger: Ledger.new)
+			@ledger = ledger
+			super()
+		end
+
 		post '/expenses' do
-			JSON.generate('expense_id': 42)
+      # Parsing the expense from the request body
+			expense = JSON.parse(request.body.read)
+      # Using the Ledger to record the expense
+      result = @ledger.record(expense)
+      # Returning a JSON document containing the result expense ID
+      JSON.generate('expense_id' => result.expense_id)
 		end
 
 		get '/expenses/:date' do
---
+++ Chapter 5/expense_tracker/spec/unit/app/api_spec.rb
@@ -0,0 +1,49 @@
+require 'rspec'
+require_relative '../../../app/api'
+
+module ExpenseTracker
+  RecordResult = Struct.new(:success?, :expense_id, :error_message)
+
+  RSpec.describe API do
+    include Rack::Test::Methods
+
+    def app
+      API.new(ledger: ledger)
+    end
+
+    # We won't define Ledger yet, as currently what we want to test is the API
+    # Instead, we'll mock its behaviour.
+    let(:ledger){ instance_double('ExpenseTracker::Ledger') }
+
+    describe 'POST /expenses' do
+      context 'when the expense is successfully recorded' do
+        # We don't need real data because we're mocking the behaviour
+        let(:expense){ {'some' => 'data'} }
+
+        before do
+          # Mocking Ledger's behaviour
+          allow(ledger).to receive(:record)
+                               .with(expense)
+                               .and_return(RecordResult.new(true, 417, nil))
+        end
+
+        it 'returns the expense id' do
+          post '/expenses', JSON.generate(expense)
+
+          parsed = JSON.parse(last_response.body)
+          expect(parsed).to include('expense_id' => 417)
+        end
+
+        it 'responds with a 200 (OK)' do
+          post '/expenses', JSON.generate(expense)
+          expect(last_response.status).to eq(200)
+        end
+      end
+
+      context 'when the expense fails validation' do
+        it 'returns an error message'
+        it 'responds with a 422 (Unprocessable entity)'
+      end
+    end
+  end
+end
