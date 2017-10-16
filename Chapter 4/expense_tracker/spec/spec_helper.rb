993fd103a96dc858f7920be05f512604f8f54de0
Alex <alexgascon.93@gmail.com>
2017-10-16 17:34:31 +0200

    Restoring Chapter 4 config to its original version

--- Chapter 4/expense_tracker/spec/spec_helper.rb
+++ Chapter 4/expense_tracker/spec/spec_helper.rb
@@ -16,11 +16,6 @@ ENV['RACK_ENV'] = 'test'
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  # Specifying which gems won't be included in the backtrace whenever we found
-  # an error on our tests. This helps us find the broken part of our code
-  # We can still see the full backtrace by adding the -b flag to RSpec
-  config.filter_gems_from_backtrace 'rack', 'rack-test', 'sequel', 'sinatra'
-
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
@@ -75,7 +70,7 @@ RSpec.configure do |config|
 
   # This setting enables warnings. It's recommended, but in some cases may
   # be too noisy due to issues in dependencies.
-  # config.warnings = true
+  config.warnings = true
 
   # Many RSpec users commonly either run the entire suite or an individual
   # file, and it's useful to allow more verbose output when running an
@@ -90,7 +85,7 @@ RSpec.configure do |config|
   # Print the 10 slowest examples and example groups at the
   # end of the spec run, to help surface which specs are running
   # particularly slow.
-  # config.profile_examples = 10
+  config.profile_examples = 10
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
