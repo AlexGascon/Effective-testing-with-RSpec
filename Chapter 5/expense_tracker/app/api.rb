5f63d7c6453943d66f4d06c9476befba96077248
Alex <alexgascon.93@gmail.com>
2017-10-17 11:43:47 +0200

    Removing unnecessary comments

--- Chapter 5/expense_tracker/app/api.rb
+++ Chapter 5/expense_tracker/app/api.rb
@@ -10,17 +10,12 @@ module ExpenseTracker
 		end
 
 		post '/expenses' do
-      # Parsing the expense from the request body
-			expense = JSON.parse(request.body.read)
-      # Using the Ledger to record the expense
+      expense = JSON.parse(request.body.read)
       result = @ledger.record(expense)
 
-      # Acting depending on the obtained result
       if result.success?
-        # Returning a JSON document containing the result expense ID
         JSON.generate('expense_id' => result.expense_id)
       else
-        # Specifically setting the error status and message
         status 422
         JSON.generate('error' => result.error_message)
       end
